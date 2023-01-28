12009fb1dfdc6f23abb601dc5b74d059733cf355
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2023-01-28 21:08:39 +0800

    Add `Sticker` support

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -4,6 +4,7 @@ class Message {
   final int uid;
   final String nickname;
   final DateTime timestamp;
+  final Sticker? sticker;
   final Medal? medal;
   final int kanchouLv;
 
@@ -12,6 +13,7 @@ class Message {
     required this.uid,
     required this.nickname,
     required this.timestamp,
+    required this.sticker,
     required this.medal,
     required this.kanchouLv,
   });
@@ -24,6 +26,7 @@ class Message {
       nickname: obj['nickname'],
       // accurate to 1 second
       timestamp: DateTime.parse(obj['timeline'] as String),
+      sticker: Sticker.fromJson(obj['emoticon']),
       medal: Medal.fromJsonArray(obj['medal'] as List),
       kanchouLv: obj['guard_level'],
     );
@@ -37,6 +40,7 @@ class Message {
       nickname: obj[2][1],
       // accurate to 1 ms (before 2038)
       timestamp: DateTime.fromMillisecondsSinceEpoch(obj[0][4] as int),
+      sticker: Sticker.fromJson(obj[0][13]),
       medal: Medal.fromJsonArray(obj[3] as List),
       kanchouLv: obj[7],
     );
@@ -64,6 +68,38 @@ class Medal {
       owner: array[2],
       level: array[0],
       color: array[9],
+    );
+  }
+}
+
+/// Represents a sticker (i.e. large emoticon) message.
+class Sticker {
+  final String id;
+  // final String? altText;
+  final String imageUrl;
+  final int width, height;
+
+  Sticker({
+    required this.id,
+    // required this.altText,
+    required this.imageUrl,
+    required this.width,
+    required this.height,
+  });
+
+  static Sticker? fromJson(dynamic obj) {
+    // Note: for non-sticker messages, when using RESTful API,
+    // obj['emoticon_unique'] will be '';
+    // when using WebSocket, the whole obj will be a string '{}'.
+    if (obj == '{}' || (obj['emoticon_unique'] as String).isEmpty) return null;
+
+    return Sticker(
+      id: obj['emoticon_unique'],
+      // This field is missing in WebSocket messages
+      // altText: obj['text'],
+      imageUrl: obj['url'],
+      width: obj['width'],
+      height: obj['height'],
     );
   }
 }
